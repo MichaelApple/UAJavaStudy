2652df4d1373f74006d254ebe01ccecd2f91fc23
Michael <Yablon_Michael@yahoo.com>
2017-07-25 12:10:43 +0300

    Added first JavaDocs

--- src/main/java/task2_3updated/Controller.java
+++ src/main/java/task2_3updated/Controller.java
@@ -9,6 +9,9 @@ import java.util.Scanner;
  */
 public class Controller {
 
+    /**
+     * Initial variables
+     */
     // Constructor
     Model model;
     View view;
@@ -16,7 +19,6 @@ public class Controller {
     int min = Model.MIN_BARRIER;
     int max = Model.MAX_BARRIER;
     int guessCount = 0;
-
     ArrayList<Integer> list = new ArrayList<>();
 
     public Controller(Model model, View view) {
@@ -24,12 +26,21 @@ public class Controller {
         this.view = view;
     }
 
+    /**
+     * @param min - minimal range number
+     * @param max - maximal range number
+     * @return Win Number in some range
+     */
     public int setRandomNumber(int min, int max) {
         Random random = new Random();
         model.setNumber(random.nextInt(max - min) + min);
         return model.getNumber();
     }
 
+    /**
+     * Method that proceed all data from model and user inputs and by using helping methods such as checkInt(Scanner sc),
+     * checkWrongNumber(Scanner sc) and method winGame() performs game logic
+     */
     // The Work method
     public void startGame(){
         Scanner sc = new Scanner(System.in);
@@ -38,16 +49,13 @@ public class Controller {
         checkInt(sc);
 
         while ((guess = sc.nextInt()) != winNumber) {
-
             checkWrongNumber(sc);
             if (guess == winNumber) break;
             if (guess > winNumber)
                 max = guess;
             else min = guess;
 
-            view.printMessage(View.WRONG_NUMBER);
-            view.printMessage(View.RANGE_OF_NUMBERS + min + " " + max);
-            view.printMessage(View.YOUR_GUESS + guess);
+            printNotificationWrongNumber();
             list.add(guess);
             guessCount++;
             checkInt(sc);
@@ -55,18 +63,24 @@ public class Controller {
         winGame();
     }
 
+    /**
+     * @param sc - user input
+     *           method get user input and validates it while input data will be entered correct
+     */
     public void checkWrongNumber(Scanner sc) {
         while (guess < min || (guess > max)) {
             list.add(guess);
             guessCount++;
-            view.printMessage(View.WRONG_NUMBER);
-            view.printMessage(View.RANGE_OF_NUMBERS + min + " " + max);
-            view.printMessage(View.YOUR_GUESS + guess);
+            printNotificationWrongNumber();
             checkInt(sc);
             guess = sc.nextInt();
         }
     }
 
+    /**
+     * @param sc - user input
+     *           method validates input if it`s not a number
+     */
     public void checkInt(Scanner sc) {
         while (!sc.hasNextInt()){
             view.printMessage(View.WRONG_INT_INPUT_DATA + View.INPUT_INT_DATA);
@@ -74,6 +88,9 @@ public class Controller {
         }
     }
 
+    /**
+     * Method that will trigger congratulations if user guessed number
+     */
     public void winGame() {
         guessCount++;
         list.add(guess);
@@ -82,5 +99,14 @@ public class Controller {
         view.printMessage(View.RANGE_OF_NUMBERS + min + " " + max);
         view.printMessage(View.NUMBER_OF_GUESSES + guessCount);
         view.printListOfGuesses(list);
+    }
+
+    /**
+     * Method's name speaks for itself )
+     */
+    private void printNotificationWrongNumber() {
+        view.printMessage(View.WRONG_NUMBER);
+        view.printMessage(View.RANGE_OF_NUMBERS + min + " " + max);
+        view.printMessage(View.YOUR_GUESS + guess);
     }
 }
