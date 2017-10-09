9ead1d11675ee6bec2d14c439c16020c2de6d230
Michael <Yablon_Michael@yahoo.com>
2017-10-09 16:28:29 +0300

    SS 11 Tasks

---
+++ src/main/java/coretasks/Game.java
@@ -0,0 +1,56 @@
+package coretasks;
+
+import moreless.View;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by Miha on 09.10.2017.
+ */
+public class Game {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.game(0, 10);
+    }
+
+    int guess;
+
+   public void game(int min, int max) {
+       Scanner sc = new Scanner(System.in);
+       int winNumber = setRandomNumber(min, max);
+       checkInt(sc);
+
+       while ((guess = sc.nextInt()) != winNumber) {
+           while (guess < min || (guess > max)) {
+               System.out.println("You didn't guess. Current range is: " + min + ": " + max);
+               checkInt(sc);
+               guess = sc.nextInt();
+           }
+           if (guess == winNumber) break;
+           if (guess > winNumber)
+               max = guess;
+           else min = guess;
+           System.out.println("You didn't guess. Current range is: " + min + ": " + max);
+           checkInt(sc);
+       }
+       System.out.println("You win!!! Number was: " + winNumber);
+       System.out.println("Your guess: " + guess);
+
+   }
+
+
+    private int setRandomNumber(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min) + min;
+    }
+
+    private void checkInt(Scanner sc) {
+        while (!sc.hasNextInt()){
+            System.out.println("Your input is not a number");
+            sc.next();
+        }
+    }
+
+}
---
+++ src/main/java/coretasks/Test.java
@@ -0,0 +1,175 @@
+package coretasks;
+
+import java.util.*;
+
+/**
+ * Created by Miha on 09.10.2017.
+ */
+public class Test {
+
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        System.out.println(test.multiply(1,2,3,4));
+
+        System.out.println(test.reverse("I am testing"));
+        System.out.println(test.is_palindrome("radar"));
+
+        test.histogram(4, 9, 7);
+
+        int[][] forward = {
+                { 1, 2, 3},
+                { 4, 5, 6},
+                { 7, 8, 9}
+        };
+
+        test.diaginal_reverse(forward);
+
+        System.out.println();
+        System.out.println(test.isBracketBalanced("[]"));
+
+        System.out.println();
+        for (Map.Entry<Character, Integer> entry : test.char_freq("abbabcbdbabdbdbabababcbcbab").entrySet())
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+
+        System.out.println();
+        test.dec_to_bin(10);
+
+    }
+
+    public void printString(String string) {
+        System.out.println("Hello, " + string + "!");
+    }
+
+    public int sum(int ... list) {
+        int result = 0;
+        for (int number : list) {
+            result += number;
+        }
+        return result;
+    }
+
+    public int multiply(int ... list) {
+        int result = 1;
+        for (int number : list) {
+            result *= number;
+        }
+        return result;
+    }
+
+    public String reverse(String string) {
+        return new StringBuilder(string).reverse().toString();
+    }
+
+
+    public boolean is_palindrome(String string) {
+        String reverse = reverse(string);
+        return string.equals(reverse);
+    }
+
+    public void histogram(int ... list) {
+        for (int item : list) {
+            for (int j = 0; j < item; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public String caesar_cipher(String plainText, int shiftKey) {
+        plainText = plainText.toLowerCase();
+
+        String cipherText = "";
+
+        for (int i = 0; i < plainText.length(); i++){
+            int charPosition = ALPHABET.indexOf(plainText.charAt(i));
+            int keyVal = (shiftKey + charPosition) % 26;
+            char replaceVal = ALPHABET.charAt(keyVal);
+            cipherText += replaceVal;
+        }
+        return cipherText;
+    }
+
+
+    public int[][] diaginal_reverse(int[][] matrix) {
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[][] reverse = new int[rows][cols];
+
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                reverse[i][j] = matrix[j][i];
+            }
+        }
+
+        // Added for visualisation
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                System.out.print(reverse[i][j]);
+                if(j < cols-1)
+                    System.out.print(", ");
+            }
+            System.out.println();
+        }
+
+        return reverse;
+    }
+
+
+    public boolean isBracketBalanced(String str) {
+
+        boolean isBalanced = false;
+        if (str.charAt(0) != '[')
+            return false;
+
+        Stack<Character> stack = new Stack<>();
+
+        char c;
+        for(int i=0; i < str.length(); i++) {
+            c = str.charAt(i);
+
+            if(c == '[')
+                stack.push(c);
+            else if(c == ']')
+                if(stack.empty())
+                    return false;
+                else if(stack.peek() == '[')
+                    stack.pop();
+                else
+                    return false;
+        }
+        return stack.empty();
+    }
+
+    public HashMap<Character,Integer> char_freq(String string) {
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < string.length(); i++){
+            char c = string.charAt(i);
+            Integer val = map.get(c);
+            if (val != null)
+                map.put(c, val + 1);
+            else
+                map.put(c,1);
+
+        }
+        return map;
+    }
+
+    public void dec_to_bin(int number) {
+        int binary[] = new int[25];
+        int index = 0;
+        while(number > 0){
+            binary[index++] = number % 2;
+            number = number / 2;
+        }
+        for(int i = index-1; i >= 0; i--){
+            System.out.print(binary[i]);
+        }
+    }
+}
